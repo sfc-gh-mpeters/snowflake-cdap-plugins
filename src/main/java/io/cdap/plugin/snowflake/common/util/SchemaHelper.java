e99667be6d748fead053a7f3d65e531549d2a246
Andrew Onishuk <aonischuk@cybervisiontech.com>
2020-03-04 09:37:27 +0200

    Snowflake Batch Source

---
+++ src/main/java/io/cdap/plugin/snowflake/common/util/SchemaHelper.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright © 2020 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.plugin.snowflake.common.util;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
+import io.cdap.cdap.api.data.schema.Schema;
+import io.cdap.cdap.etl.api.FailureCollector;
+import io.cdap.plugin.snowflake.common.client.SnowflakeAccessor;
+import io.cdap.plugin.snowflake.common.client.SnowflakeFieldDescriptor;
+import io.cdap.plugin.snowflake.common.exception.SchemaParseException;
+import io.cdap.plugin.snowflake.source.batch.SnowflakeBatchSourceConfig;
+import io.cdap.plugin.snowflake.source.batch.SnowflakeSourceAccessor;
+import java.io.IOException;
+import java.sql.Types;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * Resolves schema.
+ */
+public class SchemaHelper {
+
+  private static final Map<Integer, Schema> SNOWFLAKE_TYPE_TO_CDAP_SCHEMA =
+    new ImmutableMap.Builder<Integer, Schema>()
+      .put(Types.VARCHAR, Schema.of(Schema.Type.STRING))
+      .put(Types.CHAR, Schema.of(Schema.Type.STRING))
+      .put(Types.BINARY, Schema.of(Schema.Type.BYTES))
+      .put(Types.INTEGER, Schema.of(Schema.Type.INT))
+      .put(Types.DECIMAL, Schema.decimalOf(38, 9))
+      .put(Types.DOUBLE, Schema.of(Schema.Type.DOUBLE))
+      .put(Types.TIMESTAMP, Schema.of(Schema.LogicalType.TIMESTAMP_MICROS))
+      .put(Types.DATE, Schema.of(Schema.LogicalType.DATE))
+      .put(Types.TIME, Schema.of(Schema.LogicalType.TIME_MICROS))
+      .put(Types.BOOLEAN, Schema.of(Schema.Type.BOOLEAN))
+      .put(Types.BIGINT, Schema.decimalOf(38))
+      .put(Types.SMALLINT, Schema.decimalOf(38))
+      .build();
+
+  private SchemaHelper() {
+  }
+
+  public static Schema getSchema(SnowflakeSourceAccessor snowflakeAccessor, String schema,
+                                 FailureCollector collector, String importQuery) {
+    try {
+      if (!Strings.isNullOrEmpty(schema)) {
+        try {
+          return Schema.parseJson(schema);
+        } catch (IOException | IllegalStateException e) {
+          throw new SchemaParseException(e);
+        }
+      }
+      return getSchema(snowflakeAccessor, importQuery);
+    } catch (SchemaParseException e) {
+      collector.addFailure("Unable to retrieve output schema.",
+                           null)
+        .withConfigProperty(SnowflakeBatchSourceConfig.PROPERTY_SCHEMA);
+      return null;
+    }
+  }
+
+  public static Schema getSchema(SnowflakeAccessor snowflakeAccessor, String importQuery) {
+    try {
+      List<SnowflakeFieldDescriptor> result = snowflakeAccessor.describeQuery(importQuery);
+      List<Schema.Field> fields = result.stream()
+        .map(fieldDescriptor -> Schema.Field.of(fieldDescriptor.getName(), getSchema(fieldDescriptor)))
+        .collect(Collectors.toList());
+      return Schema.recordOf("data", fields);
+    } catch (IOException e) {
+      throw new SchemaParseException(e);
+    }
+  }
+
+  private static Schema getSchema(SnowflakeFieldDescriptor fieldDescriptor) {
+    Integer type = fieldDescriptor.getType();
+    Schema schema = SNOWFLAKE_TYPE_TO_CDAP_SCHEMA.get(type);
+    if (schema == null) {
+      throw new SchemaParseException(String.format(
+        "No corresponding Schema is found for java.sql.Type: %d", type));
+    }
+    return fieldDescriptor.getNullable()
+      ? Schema.nullableOf(schema)
+      : schema;
+  }
+
+  /**
+   * Works like {@link SchemaHelper#checkCompatibility(Schema, Schema, boolean)}
+   * except that checking nullable is always on.
+   *
+   * @see SchemaHelper#checkCompatibility(Schema, Schema, boolean)
+   */
+  public static void checkCompatibility(Schema actualSchema, Schema providedSchema) {
+    checkCompatibility(actualSchema, providedSchema, true);
+  }
+
+  /**
+   * Checks two schemas compatibility based on the following rules:
+   * <ul>
+   *   <li>Actual schema must have fields indicated in the provided schema.</li>
+   *   <li>Fields types in both schema must match.</li>
+   *   <li>If actual schema field is required, provided schema field can be required or nullable.</li>
+   *   <li>If actual schema field is nullable, provided schema field must be nullable.</li>
+   * </ul>
+   *
+   * @param actualSchema schema calculated based on Snowflake metadata information
+   * @param providedSchema schema provided in the configuration
+   * @param checkNullable if true, checks for nullability of fields in schema are triggered.
+   */
+  public static void checkCompatibility(Schema actualSchema, Schema providedSchema, boolean checkNullable) {
+    for (Schema.Field providedField : Objects.requireNonNull(providedSchema.getFields())) {
+      Schema.Field actualField = actualSchema.getField(providedField.getName(), true);
+      if (actualField == null) {
+        throw new IllegalArgumentException(
+          String.format("Field '%s' does not exist in Snowflake", providedField.getName()));
+      }
+
+      Schema providedFieldSchema = providedField.getSchema();
+      Schema actualFieldSchema = actualField.getSchema();
+
+      boolean isActualFieldNullable = actualFieldSchema.isNullable();
+      boolean isProvidedFieldNullable = providedFieldSchema.isNullable();
+
+      actualFieldSchema = isActualFieldNullable ? actualFieldSchema.getNonNullable() : actualFieldSchema;
+      providedFieldSchema = isProvidedFieldNullable ? providedFieldSchema.getNonNullable() : providedFieldSchema;
+
+      if (!schemasEquals(actualFieldSchema, providedFieldSchema)
+        || !Objects.equals(actualFieldSchema.getLogicalType(), providedFieldSchema.getLogicalType())) {
+          throw new IllegalArgumentException(
+            String.format("Expected field '%s' to be of '%s', but it is of '%s'",
+                          providedField.getName(), providedFieldSchema, actualFieldSchema)
+                          );
+      }
+
+      if (checkNullable && isActualFieldNullable && !isProvidedFieldNullable) {
+        throw new IllegalArgumentException(String.format("Field '%s' should be nullable", providedField.getName()));
+      }
+    }
+  }
+
+  private static boolean schemasEquals(Schema field1, Schema field2) {
+    // Don't compare decimal precisions. If both field are decimal type, it's good enough.
+
+    if (Objects.equals(field1.getLogicalType(), Schema.LogicalType.DECIMAL) &&
+      Objects.equals(field2.getLogicalType(), Schema.LogicalType.DECIMAL)) {
+      return true;
+    }
+
+    return field1.equals(field2);
+  }
+}
---
+++ src/main/java/io/cdap/plugin/snowflake/source/batch/SnowflakeMapToRecordTransformer.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright © 2020 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.plugin.snowflake.source.batch;
+
+import com.google.common.base.Strings;
+import io.cdap.cdap.api.common.Bytes;
+import io.cdap.cdap.api.data.format.StructuredRecord;
+import io.cdap.cdap.api.data.format.UnexpectedFormatException;
+import io.cdap.cdap.api.data.schema.Schema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Transforms Snowflake row into {@link StructuredRecord}.
+ */
+public class SnowflakeMapToRecordTransformer {
+  private static final Logger LOG = LoggerFactory.getLogger(SnowflakeMapToRecordTransformer.class);
+
+  private final Schema schema;
+
+  public SnowflakeMapToRecordTransformer(Schema schema) {
+    this.schema = schema;
+  }
+
+  public StructuredRecord transform(Map<String, String> row) {
+    return getStructuredRecord(row, schema);
+  }
+
+  private StructuredRecord getStructuredRecord(Map<String, String> row, Schema schema) {
+    StructuredRecord.Builder builder = StructuredRecord.builder(schema);
+    row.entrySet().stream()
+      .filter(entry -> schema.getField(entry.getKey()) != null) // filter absent fields in the schema
+      .forEach(entry -> builder.set(
+        entry.getKey(),
+        convertValue(entry.getKey(), entry.getValue(), schema.getField(entry.getKey()).getSchema())));
+    return builder.build();
+  }
+
+  private Object convertValue(String fieldName, Object value, Schema fieldSchema) {
+    if (fieldSchema.isNullable()) {
+      return convertValue(fieldName, value, fieldSchema.getNonNullable());
+    }
+
+    Schema.Type fieldSchemaType = fieldSchema.getType();
+
+    // empty string is considered null in csv
+    if (value == null || (value instanceof String && Strings.isNullOrEmpty((String) value))) {
+      return null;
+    }
+
+    Schema.LogicalType logicalType = fieldSchema.getLogicalType();
+    if (logicalType != null) {
+      switch (logicalType) {
+        case DATE:
+          // date will be in yyyy-mm-dd format
+          return Math.toIntExact(LocalDate.parse(String.valueOf(value)).toEpochDay());
+        case TIMESTAMP_MICROS:
+          Instant instant = DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(String.valueOf(value), Instant::from);
+          return TimeUnit.MILLISECONDS.toMicros(instant.toEpochMilli());
+        case TIME_MICROS:
+          return TimeUnit.NANOSECONDS.toMicros(LocalTime.parse(String.valueOf(value)).toNanoOfDay());
+        case DECIMAL:
+          return new BigDecimal(String.valueOf(value)).unscaledValue().toByteArray();
+        default:
+          throw new IllegalArgumentException(
+            String.format("Field '%s' is of unsupported type '%s'", fieldName, logicalType.getToken()));
+      }
+    }
+
+    switch (fieldSchemaType) {
+      case NULL:
+        return null;
+      case BYTES:
+        return Bytes.toBytes(castValue(value, fieldName, String.class));
+      case BOOLEAN:
+        return Boolean.parseBoolean(castValue(value, fieldName, String.class));
+      case DOUBLE:
+        return Double.parseDouble(castValue(value, fieldName, String.class));
+      case STRING:
+        return value;
+    }
+
+    throw new UnexpectedFormatException(
+      String.format("Unsupported schema type: '%s' for field: '%s'. Supported types are 'bytes, boolean, "
+                      + "double, string'.", fieldSchema, fieldName));
+  }
+
+  private <T> T castValue(Object value, String fieldName, Class<T> clazz) {
+    if (clazz.isAssignableFrom(value.getClass())) {
+      return clazz.cast(value);
+    }
+    throw new UnexpectedFormatException(
+      String.format("Field '%s' is not of expected type '%s'", fieldName, clazz.getSimpleName()));
+  }
+}
